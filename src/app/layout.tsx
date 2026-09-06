170f054a8e1c84c8410d2bc94bb2729821137b51
nikhil <nikhilmandlik126@gmail.com>
2026-09-06 15:53:02 +0530

    meta pixel code update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,6 +105,18 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-GX4R2NYDD0');`}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1035289126003863');
+fbq('track', 'PageView');`}
+        </Script>
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-K6SXZHSJ"
@@ -112,6 +124,15 @@ gtag('config', 'G-GX4R2NYDD0');`}
             width="0"
             style={{ display: "none", visibility: "hidden" }}
             title="Google Tag Manager"
+          />
+          {/* Meta's noscript fallback requires a plain tracking image. */}
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1035289126003863&ev=PageView&noscript=1"
+            alt=""
           />
         </noscript>
         <JsonLd data={[organizationJsonLd, websiteJsonLd]} />
